abf6e0b66e1d161ff26435becec73ce5c367fcf6
José Neto <53952575+josenetoo@users.noreply.github.com>
2024-08-28 11:52:05 -0300

    Update DemoApplication.java

--- src/main/java/DemoApplication.java
+++ src/main/java/DemoApplication.java
@@ -11,7 +11,7 @@ public class DemoApplication {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello from Spring Boot!! Oracle Java Day!!";
+        return "Hello from Spring Boot!!! Oracle Java Day!!!";
     }
 
     public static void main(String[] args) {
